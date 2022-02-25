c3f6867f2f3cdd2031908f6549dc5d65bd00f370
Rootdiv <rootdiv@mail.ru>
2022-02-25 12:48:25 +0300

    feat: opened post content and updated readme.md

--- src/components/pages/PostIdPage.vue
+++ src/components/pages/PostIdPage.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <h1>Это страница поста с ID = {{ $route.params.id }}</h1>
+    <h2>Это страница поста с ID = {{ $route.params.id }}</h2>
+    <div v-if="!isPostsLoading">
+      <br />
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
+    <div v-else>Идёт загрузка...</div>
   </div>
 </template>
 
 <script>
-  export default {};
+  import axios from 'axios';
+  export default {
+    data: () => ({ post: [], isPostsLoading: false }),
+    methods: {
+      async loadPost() {
+        try {
+          this.isPostsLoading = true;
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + this.$route.params.id);
+          this.post = response.data;
+        } catch (error) {
+          alert('Ошибка ' + error.message);
+        } finally {
+          this.isPostsLoading = false;
+        }
+      },
+    },
+    created() {
+      this.loadPost();
+    },
+  };
 </script>
-
-<style scoped>
-</style>
